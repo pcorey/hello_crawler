a0b1bcab210146d306fb298499d60d184860874d
Pete Corey <petecorey@gmail.com>
2017-10-05 15:59:52 -0400

    Basic crawler

--- lib/hello_crawler.ex
+++ lib/hello_crawler.ex
@@ -1,18 +1,45 @@
 defmodule HelloCrawler do
-  @moduledoc """
-  Documentation for HelloCrawler.
-  """
 
-  @doc """
-  Hello world.
+  @max_depth 3
 
-  ## Examples
-
-      iex> HelloCrawler.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def handle_response(url, host, path, {:ok, %{body: body}}) do
+    IO.puts("Crawling \"#{url}\"...")
+    path = [url | path]
+    [url | body
+           |> Floki.find("a")
+           |> Floki.attribute("href")
+           |> Enum.map(&URI.merge(url, &1))
+           |> Enum.map(&to_string/1)
+           |> Enum.reject(&Enum.member?(path, &1))
+           |> Enum.map(&get_links(URI.parse(&1), host, [&1 | path]))
+           |> List.flatten]
   end
+
+  def handle_response(url, _host, _path, _response) do
+    [url]
+  end
+
+  def get_links(url, _host, path) when length(path) > @max_depth do
+    [url]
+  end
+
+  def get_links(url = %{host: host}, host, path) do
+    url = to_string(url)
+    headers = []
+    options = [follow_redirect: true]
+    response = HTTPoison.get(url, headers, options)
+    handle_response(url, host, path, response)
+  end
+
+  def get_links(url, _host, _path) do
+    [url]
+  end
+
+  def get_links(url) do
+    url = URI.parse(url)
+    get_links(url, url.host, [])
+    |> Enum.map(&to_string/1)
+    |> Enum.uniq
+  end
+
 end
